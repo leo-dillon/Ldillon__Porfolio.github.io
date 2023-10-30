cb4b541a431191af1d84f0b672b68f617beb7b27
leo-dillon <jeannotegui@gmail.com>
2023-10-30 12:05:45 -0300

    Comenzando con el responsive

--- Js/main.js
+++ Js/main.js
@@ -3,29 +3,28 @@ const presentacionImg_2 = document.querySelector('.imagen_2')
 
 
 presentacionImg_1.addEventListener('click', () => {
-  presentacionImg_1.style = 'transform: translateY(-470px);'
-  presentacionImg_2.style = 'transform: translateY(-464px);'
+  let AlturaIMG1 = presentacionImg_1.height
+  console.log(AlturaIMG1)
+  presentacionImg_1.style = `transform: translateY(-${AlturaIMG1}px)`
+  presentacionImg_2.style = `transform: translateY(-${AlturaIMG1}px)`
 })
 presentacionImg_2.addEventListener('click', () => {
   presentacionImg_1.style = 'transform: translateY(0px);'
-  presentacionImg_2.style = 'transform: translateY(0    px);'
+  presentacionImg_2.style = 'transform: translateY(0px);'
 })
 
 const mainSection = document.querySelectorAll('.main__section')
 
 const cargarSection = (entradas, observador) => {
   entradas.forEach( entrada => {
-    console.log(entrada)
     if(entrada.isIntersecting){
       entrada.target.classList.add('visible')
-    }else{
-      entrada.target.classList.remove('visible')
     }
   });
 }
 const observer = new IntersectionObserver(cargarSection, {
   root: null,
-  threshold: 0.2
+  threshold: 0.1
 });
 
 mainSection.forEach((section)=>{
